0f891460fdacebde2436e702a2f29e98d7a8881c
Sandro Roger Boschetti <sandro.boschetti@ebserh.gov.br>
2023-12-07 14:22:34 -0300

    Calculos forma para uma classe; campo de capital final mudou para TextInputEditText em TextInputLayout e outras coisinhas

--- app/src/main/java/br/com/sbsys/srbjc/MainActivity.java
+++ app/src/main/java/br/com/sbsys/srbjc/MainActivity.java
@@ -15,6 +15,10 @@ import com.google.android.material.textfield.TextInputEditText;
 import java.math.RoundingMode;
 import java.text.DecimalFormat;
 import java.text.NumberFormat;
+import java.text.ParseException;
+import java.util.Locale;
+
+import br.com.sbsys.srbjc.calc.Calc;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,8 +26,21 @@ public class MainActivity extends AppCompatActivity {
     private TextInputEditText edTxNumAporte;
     private TextInputEditText edTxNumJuros;
     private TextInputEditText edTxNumTempo;
-    private EditText edTxNumResultado;
+    private TextInputEditText edTxNumCapitalFinal;
     private TextView edTxViewVersion;
+    private NumberFormat df;
+
+    public MainActivity(){
+        df = DecimalFormat.getNumberInstance(Locale.ENGLISH);
+        df.setMinimumFractionDigits(2);
+        df.setMaximumFractionDigits(2);
+        df.setRoundingMode(RoundingMode.DOWN);
+
+        //Exemplo de uso:
+        //Double teste = (Double) df.parse(nStr);
+        //String str = df.format(teste));
+        //DecimalFormat df = new DecimalFormat("#,##0.00"); //"\u00A4#,##0.00" = R$ 0.000,00
+    }
 
     private void preenchimentoInicialDosCampos(){
 
@@ -39,18 +56,22 @@ public class MainActivity extends AppCompatActivity {
         }
 
         edTxNumCapInicial = findViewById(R.id.textInputEditTextCapitalInicial);
-        edTxNumCapInicial.setText("10000");
+        //edTxNumCapInicial.setText("10000");
+        edTxNumCapInicial.setText(df.format(Double.parseDouble("10000")));
 
         edTxNumAporte = findViewById(R.id.textInputEditTextAporteMensal );
-        edTxNumAporte.setText("300");
+        //edTxNumAporte.setText("300");
+        edTxNumAporte.setText(df.format(Double.parseDouble("300")));
 
         edTxNumJuros = findViewById(R.id.textInputEditTextJurosMensal);
-        edTxNumJuros.setText("2.5");
+        //edTxNumJuros.setText("2.5");
+        edTxNumJuros.setText(df.format(Double.parseDouble("2.5")));
 
         edTxNumTempo = findViewById(R.id.textInputEditTextTempo);
-        edTxNumTempo.setText("12");
+        //edTxNumTempo.setText("12");
+        edTxNumTempo.setText(df.format(Double.parseDouble("12")));
 
-        edTxNumResultado = findViewById(R.id.editTextTextCapitalFinal);
+        edTxNumCapitalFinal = findViewById(R.id.textInputEditTextCapitalFinal);
     }
 
     @Override
@@ -61,25 +82,32 @@ public class MainActivity extends AppCompatActivity {
         preenchimentoInicialDosCampos();
     }
 
+//    public void calcular(View view){
+//        Double capInicial = Double.parseDouble(edTxNumCapInicial.getText().toString());
+//        Double aporteMensal = Double.parseDouble(edTxNumAporte.getText().toString());
+//        Double juros = Double.parseDouble(edTxNumJuros.getText().toString());
+//        Double tempo = Double.parseDouble(edTxNumTempo.getText().toString());
+//        //edTxNumResultado.setText("1000000");
+//        //Double capFinal = 1000000.00;
+//        double t = Calc.calculaTempo(Double.parseDouble("1000000"), capInicial, aporteMensal, juros / 100.0);
+//        double capFinal = Calc.calculaCapitalFinal(capInicial, aporteMensal, tempo, juros);
+//        edTxNumTempo.setText(String.valueOf(t));
+//        edTxNumResultado.setText(String.valueOf(capFinal));
+//    }
+
     public void calcular(View view){
         if(isDataFieldsValid()) {
-            Double capInicial = Double.parseDouble(edTxNumCapInicial.getText().toString());
-            Double aporteMensal = Double.parseDouble(edTxNumAporte.getText().toString());
-            Double juros = Double.parseDouble(edTxNumJuros.getText().toString());
-            Double tempo = Double.parseDouble(edTxNumTempo.getText().toString());
+            Double capInicial = Double.parseDouble(edTxNumCapInicial.getText().toString().replace(",",""));
+            Double aporteMensal = Double.parseDouble(edTxNumAporte.getText().toString().replace(",",""));
+            Double juros = Double.parseDouble(edTxNumJuros.getText().toString().replace(",",""));
+            Double tempo = Double.parseDouble(edTxNumTempo.getText().toString().replace(",",""));
 
-            Double fator = Math.pow(1 + juros / 100.0, tempo * 12.0);
-            Double capFinal = capInicial * fator + aporteMensal * (fator - 1.0) / (juros / 100.0);
+            Double capFinal = Calc.calculaCapitalFinal(capInicial, aporteMensal, tempo, juros);
 
-            NumberFormat df = DecimalFormat.getInstance();
-            df.setMinimumFractionDigits(2);
-            df.setMaximumFractionDigits(2);
-            df.setRoundingMode(RoundingMode.DOWN);
             String res = df.format(capFinal);
-
-            edTxNumResultado.setText(res);
+            edTxNumCapitalFinal.setText(res);
         }else{
-            edTxNumResultado.setText("Há campo(s) inválido(s)");
+            edTxNumCapitalFinal.setText("Há campo(s) inválido(s)");
         }
     }
 
@@ -107,40 +135,3 @@ public class MainActivity extends AppCompatActivity {
     }
 
 }
-
-
-//Código em Google Sheet Script que testei para calcular o tempo
-//    function calculaTempo(M, C, A, i) {
-//
-//        var v = 0.0;
-//        var t1 = 1.0;
-//        var t2 = 1.0;
-//
-//        while(v<M){
-//            t2 = t2 * 2.0;
-//            factor = Math.pow(1+i,t2*12);
-//            v = C * factor + A * (factor - 1.0) / i;
-//        }
-//
-//        while(v>M){
-//            t1 = t1 / 2.0;
-//            factor = Math.pow(1+i,t1*12);
-//            v = C * factor + A * (factor - 1.0) / i;
-//        }
-//
-//        var t = t1 +(t2 - t1) / 2.0;
-//
-//        while(Math.abs(v-M)>0.001){
-//            factor = Math.pow(1+i,t*12);
-//            v = C * factor + A * (factor - 1.0) / i;
-//            if(v>M){
-//                t2 = t;
-//            }
-//            if(v<M){
-//                t1 = t;
-//            }
-//            t = t1 +(t2 - t1) / 2.0;
-//        }
-//
-//        return t;
-//    }
---
+++ app/src/main/java/br/com/sbsys/srbjc/calc/Calc.java
@@ -0,0 +1,43 @@
+package br.com.sbsys.srbjc.calc;
+
+public class Calc {
+
+    public static double calculaCapitalFinal(double C, double A, double t, double i){
+        double fator = Math.pow(1 + i / 100.0, t * 12.0);
+        return C * fator + A * (fator - 1.0) / (i / 100.0);
+    }
+
+    public static double calculaTempo(double M, double C, double A, double i){
+        double v = 0.0;
+        double t1 = 1.0;
+        double t2 = 1.0;
+        double factor = 0.0;
+
+        while(v<M){
+            t2 = t2 * 2.0;
+            factor = Math.pow(1+i,t2*12);
+            v = C * factor + A * (factor - 1.0) / i;
+        }
+
+        while(v>M){
+            t1 = t1 / 2.0;
+            factor = Math.pow(1+i,t1*12);
+            v = C * factor + A * (factor - 1.0) / i;
+        }
+
+        double t = t1 +(t2 - t1) / 2.0;
+
+        while(Math.abs(v-M)>0.001){
+            factor = Math.pow(1+i,t*12);
+            v = C * factor + A * (factor - 1.0) / i;
+            if(v>M){
+                t2 = t;
+            }
+            if(v<M){
+                t1 = t;
+            }
+            t = t1 +(t2 - t1) / 2.0;
+        }
+        return t;
+    }
+}
