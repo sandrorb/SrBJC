0b63825d9a4004a25080b95d54c559ed7122eafb
Sandro Roger Boschetti <sandrorb@gmail.com>
2023-12-07 22:26:15 -0300

    Tentando por um endIcon para acionar cálculo a partir das caixas de texto.

--- app/src/main/java/br/com/sbsys/srbjc/MainActivity.java
+++ app/src/main/java/br/com/sbsys/srbjc/MainActivity.java
@@ -6,6 +6,8 @@ import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
